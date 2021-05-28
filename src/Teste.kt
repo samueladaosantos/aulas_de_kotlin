7f586d62784a279cbbff8973d61b73d97c542adb
samueladaosantos <83379137+samueladaosantos@users.noreply.github.com>
2021-05-28 20:35:56 -0300

    Exercicio de revisao aula 16

---
+++ src/Teste.kt
@@ -0,0 +1,34 @@
+import java.text.NumberFormat
+import java.util.*
+
+fun main() {
+    val nome = "Cesar Rodrigues"
+    println(nome.colocarSobrenome(lastName = "Nascimento"))
+
+    val cesarItau = ContaCorrente(1234, taxaDeOperacao = 2.0)
+    cesarItau.depositarDinheiro(102.0)
+    cesarItau.printarOSaldoAtual()
+
+    println("Digite o valor em reais:")
+    val scanner = Scanner(System.`in`)
+    val real = readLine()?.toDouble()
+    println(real?.formatarParaReal())
+
+    var idade = 3
+    println(idade.idade(3))
+}
+fun Int.idade(idadeNumero: Int) = "$this$idadeNumero"
+
+fun String.colocarSobrenome(lastName: String) = "$this $lastName"
+
+fun ContaCorrente.printarOSaldoAtual() = println("O meu saldo é ${this.saldo}")
+
+fun Double.formatarParaReal(): String {
+    val numberFormat = NumberFormat
+        .getCurrencyInstance(Locale("pt", "BR"))
+    return numberFormat.format(this)
+}
+
+fun Int.formatarInteiro() {
+    println(this)
+}
